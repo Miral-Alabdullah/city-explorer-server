c3db11295c2c6276835509a32cf92289806c5711
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-06-15 06:48:08 +0300

    not finished

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
+const express = require('express'); // require the express package
+const app = express(); // initialize your express app instance
+const weatherData = require('./data/weather.json');
 const cors = require('cors');
 
-app.use(cors()) // after you initialize your express app instance
-// a server endpoint 
+app.use(cors()); // after you initialize your express app instance
+// a server endpoint
 require('dotenv').config();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
- 
-app.listen(PORT) // kick start the express server to work
+  function (req, res) { // callback function of what we should do with our request
+    res.send('Hello World'); // our endpoint function response
+  });
+
+app.get('/weather', (req, res) => {
+  let latitude = req.query.lat;
+  let longitude = req.query.lon;
+  let cityName = req.query.city_name;
+  let CityData;
+
+  let Items = weatherData.find(item => {
+    if (item.city_name.toLowerCase() === cityName.toLowerCase() && item.lat === latitude && item.lon === longitude){
+      CityData = new Forecast(item);
+      return item; }
+  });
+  res.json(CityData);
+});
+
+
+app.listen(PORT); // kick start the express server to work
+
+
+
+class Forecast {
+  constructor(dataOfWeather) {
+    this.data = dataOfWeather.data.map(value =>{
+      let description = value.weather.description;
+      let date = value.datetime;
+      return {description, date};
+    });
+  }
+}
