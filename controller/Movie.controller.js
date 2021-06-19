e38a2f5d0023f87505de10d43bea991baee4b461
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-06-19 17:43:48 +0300

    Finshing the app

--- controller/Movie.controller.js
+++ controller/Movie.controller.js
@@ -3,22 +3,31 @@ const Movie = require('../models/Movie.model');
 require('dotenv').config();
 const MOVIES_API_KEY = process.env.MOVIES_API_KEY;
 
-const moviesController = (req, res) => {
-  let cityName = req.query.city;
-  if (cityName) {
-    const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIES_API_KEY}&query=${cityName}`;
+const Cache = require('../helper/cache');
 
-    axios.get(moviesUrl).then(response => {
-      const arrayOfMovies = [];
-      const imageUrl = `https://image.tmdb.org/t/p/w500`;
-      const responseData = response.data.results.map(obj =>
-        new Movie(obj.title, obj.overview, obj.vote_average, obj.vote_count, `${imageUrl}${obj.poster_path}`, obj.popularity, obj.release_date));
-      arrayOfMovies.push(responseData);
-      res.json(arrayOfMovies);
-      console.log(arrayOfMovies);
-    }).catch(error => {
-      res.send(error.message);
-    });
+const cacheObj = new Cache();
+
+const moviesController = (req, res) => {
+  const cityName = req.query.city;
+  const requestKey = `movies-${cityName}`;
+  if (cityName) {
+    if (cacheObj[requestKey] && (Date.now() - cacheObj[requestKey].timestamp < 86400000)) {
+      res.json(cacheObj[requestKey].data);
+    } else {
+      const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIES_API_KEY}&query=${cityName}`;
+      axios.get(moviesUrl).then(response => {
+        const arrayOfMovies = [];
+        const imageUrl = `https://image.tmdb.org/t/p/w500`;
+        const responseData = response.data.results.map(obj =>
+          new Movie(obj.title, obj.overview, obj.vote_average, obj.vote_count, `${imageUrl}${obj.poster_path}`, obj.popularity, obj.release_date));
+        cacheObj[requestKey] = responseData;
+        cacheObj[requestKey].timestamp = Date.now();
+        arrayOfMovies.push(responseData);
+        res.json(arrayOfMovies);
+      }).catch(error => {
+        res.send(error.message);
+      });
+    }
   } else {
     res.send(['You have to provide a correct city name']);
   }
